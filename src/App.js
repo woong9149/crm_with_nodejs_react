6f2f4194cceb0a6acf810e5a9b757a7f08ad0cca
woong9149 <sewoong0409@naver.com>
2020-07-30 00:33:34 +0900

    customer component 구조화

--- src/App.js
+++ src/App.js
@@ -2,21 +2,53 @@ import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-}
+const customers = [
+    {
+      'id':1,
+      'image':'https://placeimg.com/64/64/1',
+      'name':'김노드',
+      'birthday':'961222',
+      'gender':'남자',
+      'job':'대학생'
+    },
+    {
+      'id':2,
+      'image':'https://placeimg.com/64/64/2',
+      'name':'김리액',
+      'birthday':'910409',
+      'gender':'남자',
+      'job':'대학생'
+    },
+    {
+      'id':3,
+      'image':'https://placeimg.com/64/64/3',
+      'name':'문자바',
+      'birthday':'881206',
+      'gender':'남자',
+      'job':'대학생'
+    }
+]
 
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {
+        customers.map(c => {
+          return(
+              <Customer 
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          }
+        )
+      }
+    </div>
   );
 }
 
