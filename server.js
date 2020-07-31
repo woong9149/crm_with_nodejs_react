36a9f9bf4cb411eecbddb30bdbaa927bbe396e86
woong9149 <sewoong0409@naver.com>
2020-07-31 23:52:47 +0900

    rest api 구축 test

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers',(req,res) => {
+    res.send([
+        {
+            'id':1,
+            'image':'https://placeimg.com/64/64/1',
+            'name':'김노드',
+            'birthday':'961222',
+            'gender':'남자',
+            'job':'대학생'
+          },
+          {
+            'id':2,
+            'image':'https://placeimg.com/64/64/2',
+            'name':'김리액',
+            'birthday':'910409',
+            'gender':'남자',
+            'job':'대학생'
+          },
+          {
+            'id':3,
+            'image':'https://placeimg.com/64/64/3',
+            'name':'문자바',
+            'birthday':'881206',
+            'gender':'남자',
+            'job':'대학생'
+          }
+    ]);
 })
 
 
