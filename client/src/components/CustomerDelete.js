f9f8c331821c047bc906bfcca45f74604deb6a92
woong9149 <sewoong0409@naver.com>
2020-08-06 22:42:16 +0900

    Add Material UI Design for Add,Delete

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography'
 
 class CustomerDelete extends React.Component {
+
+    constructor(props){
+            super(props);
+            this.state = {
+                open: false
+            }
+        }
+    
     deleteCustomer(id){
         const url = 'api/customers/' + id;
         fetch(url,{
@@ -9,9 +23,36 @@ class CustomerDelete extends React.Component {
         this.props.stateRefresh();
     }
 
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
     render(){
         return (
-            <button onClick={ (e) => {this.deleteCustomer(this.props.id)} }>삭제</button>
+            <div>
+                <Button varint="contained"  color="secondary" onClick={ this.handleClickOpen }>삭제</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle onClose={this.handleClose}>
+                        삭제 경고
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            선택한 고객 정보가 삭제됩니다.
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button varint="contained"  color="primary" onClick={ (e) => {this.deleteCustomer(this.props.id)} }>삭제</Button>
+                        <Button varint="outlined"  color="primary" onClick={ (e) => {this.deleteCustomer(this.props.id)} }>삭제</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         )
     }
 }
